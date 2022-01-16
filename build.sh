6c8f8c60c200d26f36b8879cf64b9fc80f28f4f3
Oleksandr Hubanov <alex@0x0h.com>
2022-01-16 20:46:44 +0200

    make AllInOne build script work

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-swig2.0 -python -threads wiringpi.i
-sudo python setup.py build install
-sudo python test.py
+#!/bin/bash
+python3 generate-bindings.py > bindings.i
+swig -python wiringpi.i
+python3 setup.py clean --all
+sudo python3 setup.py build install
+sudo python3 tests/test.py
